e3be42786d169bc5c8abd921a129e2a659ad247c
M M Aminur Jibon <38575926+aminurjibon0@users.noreply.github.com>
2019-09-15 23:31:40 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+star(10);
+
+function star(rows) {
+    for(let row = 1; row < rows; row++) {
+        let pattern = '';
+        for(let i = 0; i < row; i++) {
+            pattern += '*';
+        }
+        console.log(pattern);
+    }
+}
